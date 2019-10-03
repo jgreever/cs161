cb05375222421519cfa6e8a140eadaf2d3ee2fd6
Justin Greever <jgreever@gmail.com>
2019-10-03 12:35:12 -0700

    adding all edited files

---
+++ ftoc.py
@@ -0,0 +1,4 @@
+tempf = float(input("please input your temperature in fahrenheit: "))
+tempc =  (tempf - 32)/(9/5) # convert F to C here
+print(tempf,"degrees fahrenheit is", tempc,"degrees celsius")
+
