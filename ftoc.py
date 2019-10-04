f2838b61d4b01591d69adf4db792c90777d0f95f
Justin Greever <jgreever@gmail.com>
2019-10-03 23:09:45 -0700

    corrected F to C formula

--- ftoc.py
+++ ftoc.py
@@ -1,4 +1,4 @@
 tempf = float(input("please input your temperature in fahrenheit: "))
-tempc =  (tempf - 32)/(9/5) # convert F to C here
+tempc =  (tempf - 32)*(5/9)    # convert F to C here
 print(tempf,"degrees fahrenheit is", tempc,"degrees celsius")
 
