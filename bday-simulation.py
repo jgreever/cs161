eefdcf14f7b4e012ebf2782b1ddd5c7d8059e86b
Justin Greever <jgreever@gmail.com>
2019-11-30 12:10:27 -0800

    corrected the birthday paradox code as well as finished the standard deviation code

--- bday-simulation.py
+++ bday-simulation.py
@@ -5,9 +5,9 @@ import random
 
 ft = []
 k = 0
-while(k < 365) :
-	ft.append(0)
-	k = k + 1
+while k < 365:
+    ft.append(0)
+    k = k + 1
 
 print("Please type in how many people are in the class: ")
 x = int(input())
@@ -15,29 +15,29 @@ x = int(input())
 success = 0
 
 c = 0
-while(c < 10000) :
-	k = 0
-	while(k < 365) :
-		ft[k] = 0
-		k = k + 1
+while c < 10000:
+    k = 0
+    while k < 365:
+        ft[k] = 0
+        k = k + 1
 
-	k = 0
-	while(k < x):
-		b = math.floor(364*random.random()) + 1
-		ft[b] = ft[b] + 1
-		k = k + 1
+    k = 0
+    while k < x:
+        b = math.floor(364 * random.random()) + 1
+        ft[b] = ft[b] + 1
+        k = k + 1
 
-	k = 0
-	f = 0
-	while(k < 365):
-		if(ft[k] >= 2) :
-			f = 1
-		k = k + 1
-	if(f == 1) :
-		success = success + 1
-	c = c + 1
+    k = 0
+    f = 0
+    while k < 365:
+        if ft[k] >= 2:
+            f = 1
+        k = k + 1
+    if f == 1:
+        success = success + 1
+    c = c + 1
 
 print("The probability that in a class of ", end="")
-print(x , end="")
-print(" people, at least two have the same birthday is:  " , end="")
-print(success/10000)
+print(x, end="")
+print(" people, at least two have the same birthday is:  ", end="")
+print(success / 10000)
--- standarddeviation.py
+++ standarddeviation.py
@@ -1,49 +1,46 @@
-#Standard Deviation Skeleton
+# Standard Deviation Skeleton
 
 # This program should compute the standard deviation of a sequence of non-negative numbers, terminated by a -1.
 # Standard deviation is:  A measure of how spread-out data is.
 # NOTE:  You are not allowed to use a built-in standard deviation function from the libraries.
-""" How to compute:
-1)  Find the airthmetic mean (average)
-2)  Find the difference between each data point and the mean
-3)  Square the differences
-4)  Sum the squares of the differences
-5)  Divide the sum by the number of data points
-6)  Now square root it. 
-    (import math library , to square root x: 
-    x = math.sqrt(x) )
-"""
+
 import math
 
-x = []
-n = int(input("Please enter a sequence of non-negative numbers, terminated by a -1:\n"))
-while(n >= 0) :
-	x.append(n)
-	n = int(input())
+list1 = []
+userinput = int(input("Please enter a sequence of non-negative numbers, terminated by a -1:\n"))
 
-b = len(x) - 1
-m = 0
-c = 0
-while(b >= 0) :
-	m = x[b] + m
-	b = b - 1
-	c = c + 1
-a = m/c
+while (userinput >= 0):
+    list1.append(userinput)
+    userinput = int(input())
 
-y = []
-b = len(x)
-d = 0
-t = 0
-r = 0
-while(b > 0) :
-	if(a > b) :
-		d = a - b
-	else :
-		d = b - a
-	t = math.sqrt(d)
-	r = r + t
-	b = b - 1
-o = len(x) -1
-l = r/o
-print(math.sqrt(l))
+# 1)  Find the arithmetic mean (average)
+xlen = len(list1) - 1
+counter = 0
+xplace = 0
+mavg = 0
+while (counter <= xlen):
+    mavg = mavg + list1[xplace]
+    xplace = xplace + 1
+    counter = counter + 1
+meanaverage = mavg / len(list1)
 
+#  2)  Find the difference between each data point and the mean
+list2 = []
+counter2 = 0
+xlen2 = len(list1) - 1
+xplace2 = 0
+sumsq = 0
+while (counter2 <= xlen2):
+    dpmean = abs(list1[xplace2] - meanaverage)
+    #  3)  Square the differences
+    sqdiff = math.sqrt(dpmean)
+    #  4)  Sum the squares of the differences
+    sumsq = sumsq + sqdiff
+    #  5)  Divide the sum by the number of data points
+    list2len = len(list1)
+    meanavgsq = sumsq / list2len
+    #  6)  Now square root it.
+    list2.append(dpmean)
+    counter2 = counter2 + 1
+    xplace2 = xplace2 + 1
+print("Standard Deviation: ", math.sqrt(meanavgsq))
